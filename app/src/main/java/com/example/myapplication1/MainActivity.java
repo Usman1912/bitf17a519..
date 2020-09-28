92420ef117977466751fb525b5332633da98f613
Usman Javed <bitf17a519@pucit.edu.pk>
2020-09-27 17:10:04 -0700

    commit5

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "whats up", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Good", Toast.LENGTH_SHORT).show();
     }
 }
